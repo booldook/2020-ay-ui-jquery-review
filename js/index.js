00d00bdb57afcdb4eab30b28a40a134549396488
booldook <booldook@gmail.com>
2020-01-23 11:25:44 +0900

    index

--- js/index.js
+++ js/index.js
@@ -32,13 +32,23 @@ $(".box").css({"color": "red"}) // Setter
 1. attr("src"), attr("src", "./img/a.jpg"), data(), prop()
 */
 
-$(".navi").mouseenter(subShow);	//메인네비에 마우스 올렸을때 서브 나타나기
-$(".navi").mouseleave(subHide);	//메인네비에 마우스 올렸을때 서브 사라지기
+$(window).resize(onResize).trigger("resize");
+$(".navi").mouseenter(onSubShow);	//메인네비에 마우스 올렸을때 서브 나타나기
+$(".navi").mouseleave(onSubHide);	//메인네비에 마우스 올렸을때 서브 사라지기
+$(".navi-mobile").click(onMobileSubToggle);	//모바일에서 햄버거 클릭시...
 
-function subShow() {
+function onMobileSubToggle() {
+	$(".sub-mobile").stop().fadeToggle(300);
+}
+
+function onSubShow() {
 	$(this).find(".sub-wrap").stop().fadeIn(300);
 }
 
-function subHide() {
+function onSubHide() {
 	$(this).find(".sub-wrap").stop().fadeOut(300);
+}
+
+function onResize() {
+	
 }
