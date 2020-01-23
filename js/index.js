d93182cbf78dc6d70814ff2c2a110d4e178570dd
booldook <booldook@gmail.com>
2020-01-23 12:55:06 +0900

    index.html

--- js/index.js
+++ js/index.js
@@ -1,40 +1,22 @@
 /*
 ## 접근자 - $("객체명")
-index(), eq(), find(), children(), sibling(), parent(), next(), prev()
-
+index(), eq(), sibling(),
 ## 이벤트
-click, mouseover, mouseleave, scroll, resize, keypress, keyup
+scroll, keypress, keyup
 on(), off(), one(), trigger(), delegate(), undelegate(), live(), die()
-
 ## Animation
 1. animate({css}, 속도, easing, cb)
-2. fadeIn(), fadeOut(), fadeToggle() 
-3. hide(), show(), toggle()
-4. slideUp(), slideDown(), slideToggle()
-5. stop(), delay(), clearQueue()
-
+5. delay(), clearQueue()
 ## DOM
 1. 생성
 append(), appendTo(), prepend(), prependTo(), clone(), html(), text(), insertAfter(), insertBefore(), after(), before(), wrapAll(),
 2. 삭제
 empty(), remove()
 3. 반복: each()
-
 ## css관련
-1. addClass(), removeClass(), toggleClass(), hasClass();
-2. css("color"), css({"color": "red"}), css("color", "red")
-~~~js
-var color = $(".box").css("color"); // Getter
-$(".box").css({"color": "red"}) // Setter
-~~~
-
+1. hasClass();
 ## 속성관련
 1. attr("src"), attr("src", "./img/a.jpg"), data(), prop()
-
-$(객체).width() - margin, border, padding을 제외한 순수 컨텐츠 사이즈
-$(객체).innerWidth() - margin, border를 제외한 padding + content
-$(객체).outerWidth() - margin을 제외한 border + padding + content
-$(객체).outerWidth(true) - margin + border + padding + content
 */
 
 $(window).resize(onResize).trigger("resize");
@@ -58,7 +40,9 @@ function onMobileListToggle() {
 }
 
 function onMobileSubToggle() {
-	$(".sub-mobile").stop().fadeToggle(300);
+	$(".sub-mobile").stop().fadeToggle(300, function(){
+		if(!$(this).is(":visible")) initMobileSub();
+	});
 }
 
 function onSubShow() {
@@ -71,12 +55,17 @@ function onSubHide() {
 
 function onResize() {
 	var wid = $(window).outerWidth();
-
-	$(".sub-mobile").hide();
+	initMobileSub();
 	if(wid <= 768) {
 
 	}
 	if(wid <= 576) {
 		
 	}
+}
+
+function initMobileSub() {
+	$(".sub-mobile").hide();
+	$(".sub-wrap").hide();
+	$(".subs").find(".fa-angle-down").removeClass("down");
 }
